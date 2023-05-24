535bdc5249ff0e6d5a1b3073b73785c11d3ad73b
felltrin <luciholzinger@gmail.com>
2023-05-24 17:09:15 +0000

    added api endpoint to view all json objects

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask
+from flask import Flask, request
 from psycopg2.extras import RealDictCursor
 
 conn = psycopg2.connect(
@@ -8,6 +8,16 @@ conn = psycopg2.connect(
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return "Hello, World!"
+    first_name = request.args.get("first_name", "googly")
+    last_name = request.args.get("last_name", "mcfoogly")
+    return f"<h1>Hello, {first_name} {last_name}!</h1>"
+
+@app.route("/api/chip")
+def fetch_chip():
+    with conn.cursor() as cur:
+        cur.execute("select * from chip")
+        result = list(cur.fetchall())
+
+        return result
